fb8cbbdcaccb7bca6a898b9f5e3e36f8c1687a95
joypaul1 <joypaul.bd1997@gmail.com>
2023-06-15 17:11:25 +0600

    user role list

---
+++ action/role_permission/user_role.php
@@ -0,0 +1,203 @@
+<?php
+header('Content-type: application/json; charset=UTF-8');
+
+require_once('../../inc/config.php');
+
+$baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
+$basePath =  $baseUrl . '/rHR';
+
+session_start();
+$response = array();
+
+
+// Handle Create request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' &&  trim($_POST["actionType"]) == 'create') {
+
+    $get_role_id = ($_POST["role_id"]);
+    $get_permission_id = $_POST['permission_id'];
+
+    if (empty($get_role_id) && count($get_permission_id)> 0) {
+        $message = [
+            'text' => 'Please Select Role & Permission Data!',
+            'status' => 'false',
+        ];
+        $_SESSION['noti_message'] = $message;
+        header("location:" . $basePath . "/role_permission/role_permission/create.php");
+        exit();
+    } else {
+        try {
+            $conn_hr->begin_transaction();
+            $sql = "INSERT INTO  tbl_roles_permissions ( role_id, permission_id) VALUES (?, ?)";
+            if ($stmt = mysqli_prepare($conn_hr, $sql)) {
+
+                mysqli_stmt_bind_param($stmt, "ss", $param_role_id, $param_permission_id); // Bind variables to the prepared statement as parameters
+                foreach ($get_permission_id as $key => $permission_id) {
+                                    // Set parameters
+                    $param_role_id = $get_role_id;
+                    $param_permission_id = $permission_id;
+                    // Attempt to execute the prepared statement
+                    $result =mysqli_stmt_execute($stmt);
+                    if (!$result) {
+                        $message = [
+                            'text' =>  "Oops! Something went wrong. Please try again later.",
+                            'status' => 'false',
+                        ];
+                        $_SESSION['noti_message'] = $message;
+                        header("location:" . $basePath . "/role_permission/role_permission/create.php");
+                     
+                    } 
+                }
+            }
+            mysqli_stmt_close($stmt);  // Close statement
+            $conn_hr->commit();
+        } catch (Exception $e) {
+            $conn_hr->rollback();  
+            // Handle the exception
+            $message = [
+                'text' =>   $e->getMessage(),
+                'status' => 'false',
+            ];
+            $_SESSION['noti_message'] = $message;
+            header("location:" . $basePath . "/role_permission/role_permission/create.php");
+            exit();
+        }
+    }
+    mysqli_close($conn_hr); // Close connection
+    $message = [
+        'text' => 'Data Created Successfully.',
+        'status' => 'true',
+    ];
+    $_SESSION['noti_message'] = $message;
+    header("location:" . $basePath . "/role_permission/role_permission/index.php");
+    exit();
+  
+}
+
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' &&  trim($_POST["actionType"]) == 'update') {
+
+    // $name = trim($_POST["name"]);
+    $role_id  = trim($_GET["editID"]);
+    $deleteID  = [];
+    $insertID  = [];
+    $getRoleWisePermission = getRoleWisePermission($conn_hr, $role_id);
+    $newPermission_id = $_REQUEST['permission_id'];
+    $oldPermission_id =  array_column($getRoleWisePermission, 'permission_id');
+
+    $deleteID = array_diff($oldPermission_id, $newPermission_id); // form tbl_role_permission
+    $insertID = array_diff($newPermission_id, $oldPermission_id); // form tbl_role_permission
+
+    // Begin the database transaction
+    try {
+        $conn_hr->begin_transaction();
+        //delete data from database
+        if (count($deleteID) > 0) {
+            foreach ($deleteID as $key => $perID) {
+                $sql = "DELETE FROM tbl_roles_permissions  WHERE role_id = $role_id AND permission_id = $perID";
+                $result = $conn_hr->query($sql);
+                if ($result != TRUE) {
+                    $message = [
+                        'text' =>  $conn_hr->error,
+                        'status' => 'false',
+                    ];
+                    $_SESSION['noti_message'] = $message;
+                    header("location:" . $basePath . "/role_permission/role_permissions/index.php");
+                }
+            }
+        }  //end delete data from database
+
+        //insert data from database
+        if (count($insertID) > 0) {
+            foreach ($insertID as $key => $inID) {
+                $sql = "INSERT INTO tbl_roles_permissions (role_id , permission_id)  VALUES  ($role_id , $inID)";
+                $result = $conn_hr->query($sql);
+                if ($result != TRUE) {
+                    $message = [
+                        'text' =>  $conn_hr->error,
+                        'status' => 'false',
+                    ];
+                    $_SESSION['noti_message'] = $message;
+                    header("location:" . $basePath . "/role_permission/role_permission/edit.php?id=" . $role_id . "&amp;&amp;actionType=edit");
+                }
+            }
+        }  //end insert data from database
+
+        $conn_hr->commit();
+    } catch (Exception $e) {
+        $conn_hr->rollback();
+        echo 'An error occurred: ' . $e->getMessage();  // Handle the exception
+        exit();
+    }
+    $conn_hr->close();
+    $message = [
+        'text' => 'Data Updated Successfully.',
+        'status' => 'true',
+    ];
+    $_SESSION['noti_message'] = $message;
+    header("location:" . $basePath . "/role_permission/role_permission/index.php");
+    exit();
+}
+
+if (($_GET["deleteID"])) {
+
+    // Prepare a delete statement
+    $sql = "DELETE FROM tbl_roles_permissions WHERE id = ?";
+
+
+    if ($stmt = mysqli_prepare($conn_hr, $sql)) {
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+
+        // Set parameters
+        $param_id = intval($_GET['deleteID']);
+
+        // Attempt to execute the prepared statement
+        if (mysqli_stmt_execute($stmt)) {
+
+            $response['status']  = 'success';
+            $response['message'] = 'Deleted Successfully1111 ...';
+            echo json_encode($response);
+            exit();
+        } else {
+            $response['status']  = 'error';
+            $response['message'] = 'Something went wrong! Please try again';
+            echo json_encode($response);
+        }
+    }
+
+    // Close statement
+    mysqli_stmt_close($stmt);
+
+    // Close connection
+    mysqli_close($conn_hr);
+} else {
+    // Check existence of id parameter
+    if (empty(trim($_GET["deleteID"]))) {
+        // URL doesn't contain id parameter. Redirect to error page
+        $response['status']  = 'error';
+        $response['message'] = "URL doesn't contain id parameter!";
+        echo json_encode($response);
+        header("location:" . $basePath . "/role_permission/role_permissions/index.php");
+        exit();
+    }
+}
+
+
+
+function getRoleWisePermission($conn_hr, $role_id)
+{
+    $permissionArray = [];
+    $sql        = "SELECT * FROM tbl_roles_permissions Where role_id =" . $role_id; //  select query execution
+    $perResult     = mysqli_query($conn_hr, $sql);
+    // Loop through the fetched rows
+    if ($perResult) {
+        while ($row = mysqli_fetch_array($perResult)) {
+            $permissionArray[] = array(
+                'role_id' => $row['role_id'],
+                'permission_id' => $row['permission_id']
+            );
+        }
+    }
+    return $permissionArray;
+}
---
+++ role_permission/user_role/index.php
@@ -0,0 +1,171 @@
+<?php
+$v_page        = 'role';
+$v_active_open = 'active open';
+$v_active      = 'active';
+
+require_once('../../helper/com_conn.php');
+
+
+// Initialize an empty array
+$dataArray = array();
+
+$num_per_page = 10;
+if (isset($_GET["page"])) {
+    $page = $_GET["page"];
+} else {
+    $page = 1;
+}
+
+$start_from = ($page - 1) * $num_per_page;
+// Retrieve user-wise roles
+$sql = "SELECT u.id as u_id, u.first_name as u_name,  GROUP_CONCAT(r.name) AS roles
+        FROM tbl_users u
+        LEFT JOIN tbl_users_roles ur ON u.id = ur.user_id
+        LEFT JOIN tbl_roles r ON ur.role_id = r.id
+        GROUP BY u.id, u.first_name";
+$result = $conn_hr->query($sql);
+if ($result) {
+    // Process the query result
+    while ($row = $result->fetch_assoc()) {
+        // Access the retrieved data
+        $dataArray [] = [
+            'u_id' => $row['u_id'],
+            'u_name' => $row['u_name'],
+            'roles' => $row['roles'],
+
+        ];
+        // echo "User ID: " . $row["u_id"] . ", Username: " . $row["u_name"] . ", Role Name: " . $row["roles"] . "<br>";
+    }
+} else {
+    echo "Error executing query: " . $conn_hr->error;
+}
+
+
+?>
+
+<!-- / Content -->
+
+<div class="container-xxl flex-grow-1 container-p-y">
+
+    <div class="row">
+        <div class="col-lg-12">
+
+            <div class="card border-top">
+                <!-- table header -->
+                <?php
+                $leftSideName  = 'User Role List';
+                $rightSideName = 'User Role Create';
+                $routePath     = 'role_permission/use_role/create.php';
+                include('../../layouts/_tableHeader.php');
+
+                ?>
+                <!-- End table  header -->
+
+
+
+                <div class="card-body">
+                    <div class="table-responsive text-nowrap">
+                        <table class=" table table-bordered text-center dataTable">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>User Name</th>
+                                    <th>Role Name</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+
+                                <?php
+
+                                foreach ($dataArray as $key => $row) {
+                                    
+                                    echo "<tr>";
+                                    echo "<td>" .  $key + 1 . "</td>";
+                                    echo "<td>" . $row['u_name'] . "</td>";
+                                    echo "<td>" . $row['roles'] . "</td>";
+                                    echo "<td>";
+                                    echo '<a href="' . $basePath . '/role_permission/role/edit.php?id=' . $row['u_id'] . '&amp;&amp;actionType=edit" class="btn btn-sm btn-secondary flo~at-right"> <i class="bx bx-edit-alt me-1"></i></a>';
+                                    echo ' <button data-id="' . $row['u_id'] . '" data-href="' . $basePath . '/' . 'action/role_permission/role.php" type="button" class="btn btn-sm btn-danger float-right delete_check"><i class="bx bx-trash-alt me-1"></i> </button>';
+                                    echo "</tr>";
+                                }
+
+                                ?>
+
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+                <div class="card-title">
+
+
+                    <?php
+                    // require_once('../../helper/pagination.php');
+                    // echo generatePagination($tableName, $page, $num_per_page);
+
+                    mysqli_close($conn_hr)
+                    ?>
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+
+
+<!-- / Content -->
+
+<?php require_once('../../layouts/footer_info.php'); ?>
+<?php require_once('../../layouts/footer.php'); ?>
+<script>
+    //delete data processing
+
+    $(document).on('click', '.delete_check', function() {
+        var id = $(this).data('id');
+        let url = $(this).data('href');
+        swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!',
+        }).then((result) => {
+            if (result.value) {
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                        url: url,
+                        type: 'GET',
+                        data: {
+                            deleteID: id
+                        },
+                        dataType: 'json'
+                    })
+                    .done(function(response) {
+                        console.log(response);
+                        swal.fire('Deleted!', response.message, response.status);
+                        location.reload(); // Reload the page
+                    })
+                    .fail(function() {
+                        swal.fire('Oops...', 'Something went wrong!', 'error');
+                    });
+
+            }
+
+        })
+
+    });
+</script>
