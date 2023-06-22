16dc8ad17260df645989052f8f107f21b0d70f85
joypaul1 <joypaul.bd1997@gmail.com>
2023-06-22 16:21:54 +0600

    d

--- home/dashboard_hr.php
+++ home/dashboard_hr.php
@@ -230,8 +230,7 @@ while ($row = oci_fetch_assoc($joinSQL)) {
                         </div>
                     </div>
                 </div>
-                <!-- </div>
-    <div class="row"> -->
+                
                 <div class="col-12 mb-4">
                     <div class="card">
                         <div class="card-body">
