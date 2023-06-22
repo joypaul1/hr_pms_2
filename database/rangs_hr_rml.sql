676cbe832c79f3b1c1107f4ebec520713ebebe75
joypaul1 <joypaul.bd1997@gmail.com>
2023-06-22 18:23:52 +0600

    update data

--- database/rangs_hr_rml.sql
+++ database/rangs_hr_rml.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2023 at 01:52 PM
+-- Generation Time: Jun 22, 2023 at 02:23 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.1.17
 
@@ -56,6 +56,7 @@ CREATE TABLE `tbl_permissions` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `name` varchar(255) NOT NULL,
   `slug` varchar(255) NOT NULL,
+  `permission_module_id` bigint(20) UNSIGNED DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -64,53 +65,109 @@ CREATE TABLE `tbl_permissions` (
 -- Dumping data for table `tbl_permissions`
 --
 
-INSERT INTO `tbl_permissions` (`id`, `name`, `slug`, `created_at`, `updated_at`) VALUES
-(1, 'Role-List', 'role-list', '2023-06-01 00:46:25', '2023-06-01 00:46:25'),
-(2, 'Role-Create', 'role-create', '2023-06-01 00:46:29', '2023-06-01 00:46:29'),
-(3, 'Role-Edit', 'role-edit', '2023-06-01 00:46:32', '2023-06-01 00:46:32'),
-(4, 'Role-Delete', 'role-delete', '2023-06-01 00:46:36', '2023-06-01 00:46:36'),
-(5, 'Permission-List', 'permission-list', '2023-06-01 00:46:56', '2023-06-01 00:46:56'),
-(6, 'Permission-Create', 'permission-create', '2023-06-01 00:47:05', '2023-06-01 00:47:05'),
-(7, 'Permission-Edit', 'permission-edit', '2023-06-01 00:47:09', '2023-06-01 00:47:09'),
-(8, 'Permission-Delete', 'permission-delete', '2023-06-01 00:47:13', '2023-06-01 00:47:13'),
-(9, 'Role-Permission-List', 'role-permission-list', '2023-06-01 00:47:32', '2023-06-01 00:47:32'),
-(10, 'Role-Permission-Create', 'role-permission-create', '2023-06-01 00:47:35', '2023-06-01 00:47:35'),
-(11, 'Role-Permission-Edit', 'role-permission-edit', '2023-06-01 00:47:38', '2023-06-01 00:47:38'),
-(12, 'Role-Permission-Delete', 'role-permission-delete', '2023-06-01 00:47:42', '2023-06-01 00:47:42'),
-(13, 'User-Role-List', 'user-role-list', '2023-06-01 00:48:23', '2023-06-01 00:48:23'),
-(14, 'User-Role-Create', 'user-role-create', '2023-06-01 00:48:52', '2023-06-01 00:48:52'),
-(15, 'User-Role-Edit', 'user-role-edit', '2023-06-01 00:48:55', '2023-06-01 00:48:55'),
-(16, 'User-Role-Delete', 'user-role-delete', '2023-06-01 00:48:58', '2023-06-01 00:48:58'),
-(17, 'PMS-List', 'pms-list', '2023-06-04 03:19:19', '2023-06-04 03:19:19'),
-(18, 'PMS-Create', 'pms-create', '2023-06-04 03:19:22', '2023-06-04 03:19:22'),
-(19, 'PMS-Edit', 'pms-edit', '2023-06-04 03:19:31', '2023-06-04 03:19:31'),
-(20, 'PMS-Delete', 'pms-delete', '2023-06-04 03:19:38', '2023-06-04 03:19:38'),
-(21, 'PMS-Year-List', 'pms-year-list', '2023-06-04 03:19:51', '2023-06-04 03:19:51'),
-(22, 'PMS-Year-Create', 'pms-year-create', '2023-06-04 03:19:54', '2023-06-04 03:19:54'),
-(23, 'PMS-Year-Edit', 'pms-year-edit', '2023-06-04 03:20:00', '2023-06-04 03:20:00'),
-(24, 'PMS-Year-Delete', 'pms-year-delete', '2023-06-04 03:20:04', '2023-06-04 03:20:04'),
-(25, 'PMS-KRA-List', 'pms-kra-list', '2023-06-04 03:22:12', '2023-06-04 03:22:12'),
-(26, 'PMS-KRA-Create', 'pms-kra-create', '2023-06-04 03:22:15', '2023-06-04 03:22:15'),
-(27, 'PMS-KRA-Edit', 'pms-kra-edit', '2023-06-04 03:22:18', '2023-06-04 03:22:18'),
-(28, 'PMS-KRA-Delete', 'pms-kra-delete', '2023-06-04 03:22:23', '2023-06-04 03:22:23'),
-(29, 'PMS-KPI-List', 'pms-kpi-list', '2023-06-04 03:22:34', '2023-06-04 03:22:34'),
-(30, 'PMS-KPI-Create', 'pms-kpi-create', '2023-06-04 03:22:37', '2023-06-04 03:22:37'),
-(31, 'PMS-KPI-Edit', 'pms-kpi-edit', '2023-06-04 03:22:41', '2023-06-04 03:22:41'),
-(32, 'PMS-KPI-Delete', 'pms-kpi-delete', '2023-06-04 03:23:43', '2023-06-04 03:23:43'),
-(33, 'Report -Outdoor- Attendance', 'report-outdoor-attendance', '2023-06-04 03:32:05', '2023-06-04 03:36:28'),
-(34, 'Report -Self- Attendance', 'report-self-attendance', '2023-06-04 04:01:35', '2023-06-04 04:01:35'),
-(35, 'Tour-List', 'tour-list', '2023-06-04 04:01:44', '2023-06-04 04:01:54'),
-(36, 'Tour-Create', 'tour-create', '2023-06-04 04:01:58', '2023-06-04 04:01:58'),
-(37, 'Tour-Edit', 'tour-edit', '2023-06-04 04:02:02', '2023-06-04 04:02:02'),
-(38, 'Tour-Delete', 'tour-delete', '2023-06-04 04:02:06', '2023-06-04 04:02:06'),
-(39, 'Leave-List', 'leave-list', '2023-06-04 04:02:55', '2023-06-04 04:02:55'),
-(40, 'Leave-Create', 'leave-create', '2023-06-04 04:02:58', '2023-06-04 04:02:58'),
-(41, 'Leave-Edit', 'leave-edit', '2023-06-04 04:03:02', '2023-06-04 04:03:02'),
-(42, 'Leave-Delete', 'leave-delete', '2023-06-04 04:03:05', '2023-06-04 04:03:05'),
-(43, 'User-List', 'user-list', '2023-06-04 04:38:11', '2023-06-04 04:38:11'),
-(44, 'User-Create', 'user-create', '2023-06-04 04:38:18', '2023-06-04 04:38:18'),
-(45, 'User-Edit', 'user-edit', '2023-06-04 04:40:35', '2023-06-04 04:40:35'),
-(46, 'User-Delete', 'user-delete', '2023-06-04 04:40:36', '2023-06-04 04:40:36');
+INSERT INTO `tbl_permissions` (`id`, `name`, `slug`, `permission_module_id`, `created_at`, `updated_at`) VALUES
+(1, 'Role-List', 'role-list', 1, '2023-06-01 00:46:25', '2023-06-01 00:46:25'),
+(2, 'Role-Create', 'role-create', 1, '2023-06-01 00:46:29', '2023-06-01 00:46:29'),
+(3, 'Role-Edit', 'role-edit', 1, '2023-06-01 00:46:32', '2023-06-01 00:46:32'),
+(4, 'Role-Delete', 'role-delete', 1, '2023-06-01 00:46:36', '2023-06-01 00:46:36'),
+(5, 'Permission-List', 'permission-list', 1, '2023-06-01 00:46:56', '2023-06-01 00:46:56'),
+(6, 'Permission-Create', 'permission-create', 1, '2023-06-01 00:47:05', '2023-06-01 00:47:05'),
+(7, 'Permission-Edit', 'permission-edit', 1, '2023-06-01 00:47:09', '2023-06-01 00:47:09'),
+(8, 'Permission-Delete', 'permission-delete', 1, '2023-06-01 00:47:13', '2023-06-01 00:47:13'),
+(9, 'Role-Permission-List', 'role-permission-list', 1, '2023-06-01 00:47:32', '2023-06-01 00:47:32'),
+(10, 'Role-Permission-Create', 'role-permission-create', 1, '2023-06-01 00:47:35', '2023-06-01 00:47:35'),
+(11, 'Role-Permission-Edit', 'role-permission-edit', 1, '2023-06-01 00:47:38', '2023-06-01 00:47:38'),
+(12, 'Role-Permission-Delete', 'role-permission-delete', 1, '2023-06-01 00:47:42', '2023-06-01 00:47:42'),
+(13, 'User-Role-List', 'user-role-list', 1, '2023-06-01 00:48:23', '2023-06-01 00:48:23'),
+(14, 'User-Role-Create', 'user-role-create', 1, '2023-06-01 00:48:52', '2023-06-01 00:48:52'),
+(15, 'User-Role-Edit', 'user-role-edit', 1, '2023-06-01 00:48:55', '2023-06-01 00:48:55'),
+(16, 'User-Role-Delete', 'user-role-delete', 1, '2023-06-01 00:48:58', '2023-06-01 00:48:58'),
+(17, 'PMS-List', 'pms-list', 5, '2023-06-04 03:19:19', '2023-06-04 03:19:19'),
+(18, 'PMS-Create', 'pms-create', 5, '2023-06-04 03:19:22', '2023-06-04 03:19:22'),
+(19, 'PMS-Edit', 'pms-edit', 5, '2023-06-04 03:19:31', '2023-06-04 03:19:31'),
+(20, 'PMS-Delete', 'pms-delete', 5, '2023-06-04 03:19:38', '2023-06-04 03:19:38'),
+(21, 'PMS-Year-List', 'pms-year-list', 5, '2023-06-04 03:19:51', '2023-06-04 03:19:51'),
+(22, 'PMS-Year-Create', 'pms-year-create', 5, '2023-06-04 03:19:54', '2023-06-04 03:19:54'),
+(23, 'PMS-Year-Edit', 'pms-year-edit', 5, '2023-06-04 03:20:00', '2023-06-04 03:20:00'),
+(24, 'PMS-Year-Delete', 'pms-year-delete', 5, '2023-06-04 03:20:04', '2023-06-04 03:20:04'),
+(25, 'PMS-KRA-List', 'pms-kra-list', 5, '2023-06-04 03:22:12', '2023-06-04 03:22:12'),
+(26, 'PMS-KRA-Create', 'pms-kra-create', 5, '2023-06-04 03:22:15', '2023-06-04 03:22:15'),
+(27, 'PMS-KRA-Edit', 'pms-kra-edit', 5, '2023-06-04 03:22:18', '2023-06-04 03:22:18'),
+(28, 'PMS-KRA-Delete', 'pms-kra-delete', 5, '2023-06-04 03:22:23', '2023-06-04 03:22:23'),
+(29, 'PMS-KPI-List', 'pms-kpi-list', 5, '2023-06-04 03:22:34', '2023-06-04 03:22:34'),
+(30, 'PMS-KPI-Create', 'pms-kpi-create', 5, '2023-06-04 03:22:37', '2023-06-04 03:22:37'),
+(31, 'PMS-KPI-Edit', 'pms-kpi-edit', 5, '2023-06-04 03:22:41', '2023-06-04 03:22:41'),
+(32, 'PMS-KPI-Delete', 'pms-kpi-delete', 5, '2023-06-04 03:23:43', '2023-06-04 03:23:43'),
+(35, 'Tour-List', 'tour-list', 3, '2023-06-04 04:01:44', '2023-06-04 04:01:54'),
+(36, 'Tour-Create', 'tour-create', 3, '2023-06-04 04:01:58', '2023-06-04 04:01:58'),
+(37, 'Tour-Edit', 'tour-edit', 3, '2023-06-04 04:02:02', '2023-06-04 04:02:02'),
+(38, 'Tour-Delete', 'tour-delete', 3, '2023-06-04 04:02:06', '2023-06-04 04:02:06'),
+(39, 'Self-Leave-List', 'self-leave-list', 2, '2023-06-04 04:02:55', '2023-06-04 04:02:55'),
+(40, 'Self-Leave-Create', 'self-leave-create', 2, '2023-06-04 04:02:58', '2023-06-04 04:02:58'),
+(41, 'Self-Leave-Edit', 'self-leave-edit', 2, '2023-06-04 04:03:02', '2023-06-04 04:03:02'),
+(42, 'Self-Leave-Delete', 'self-leave-delete', 2, '2023-06-04 04:03:05', '2023-06-04 04:03:05'),
+(51, 'HR-Leave-List', 'hr-leave-list', 2, NULL, NULL),
+(52, 'HR-Leave-Create', 'hr-leave-create', 2, NULL, NULL),
+(53, 'HR-Leave-Edit', 'hr-leave-edit', 2, NULL, NULL),
+(54, 'HR-Leave-Delete', 'hr-leave-delete', 2, NULL, NULL),
+(55, 'LM-Leave-List', 'lm-leave-list', 2, NULL, NULL),
+(56, 'LM-Leave-Create', 'lm-leave-create', 2, NULL, NULL),
+(57, 'LM-Leave-Edit', 'lm-leave-edit', 2, NULL, NULL),
+(58, 'LM-Leave-Delete', 'lm-leave-delete', 2, NULL, NULL),
+(59, 'Concern-Leave-List', 'concern-leave-list', 2, NULL, NULL),
+(60, 'Concern-Leave-Create', 'concern-leave-create', 2, NULL, NULL),
+(61, 'Concern-Leave-Edit', 'concern-leave-edit', 2, NULL, NULL),
+(62, 'Concern-Leave-Delete', 'concern-leave-delete', 2, NULL, NULL),
+(63, 'Self-Tour-Report', 'self-tour-report', 3, NULL, NULL),
+(64, 'Self-Tour-Create', 'self-tour-create', 3, NULL, NULL),
+(65, 'Self-Tour-Edit', 'self-tour-edit', 3, NULL, NULL),
+(66, 'Self-Tour-Delete', 'self-tour-delete', 3, NULL, NULL),
+(67, 'HR-Tour-Report', 'hr-tour-report', 3, NULL, NULL),
+(68, 'HR-Tour-Create', 'hr-tour-create', 3, NULL, NULL),
+(69, 'HR-Tour-Edit', 'hr-tour-edit', 3, NULL, NULL),
+(70, 'HR-Tour-Delete', 'hr-tour-delete', 3, NULL, NULL),
+(71, 'LM-Tour-Report', 'lm-tour-report', 3, NULL, NULL),
+(72, 'LM-Tour-Create', 'lm-tour-create', 3, NULL, NULL),
+(73, 'LM-Tour-Edit', 'lm-tour-edit', 3, NULL, NULL),
+(74, 'LM-Tour-Delete', 'lm-tour-delete', 3, NULL, NULL),
+(75, 'Concern-Tour-Report', 'concern-tour-report', 3, NULL, NULL),
+(76, 'Concern-Tour-Create', 'concern-tour-create', 3, NULL, NULL),
+(77, 'Concern-Tour-Edit', 'concern-tour-edit', 3, NULL, NULL),
+(78, 'Concern-Tour-Delete', 'concern-tour-delete', 3, NULL, NULL),
+(80, 'Roster-Create', 'roster-create', 7, NULL, NULL),
+(81, 'Roster-List', 'roster-list', 7, NULL, NULL),
+(82, 'Self-Attendance-Report', 'self-attendance-report', 4, NULL, NULL),
+(83, 'HR-Attendance-Create', 'hr-attendance-create', 4, NULL, NULL),
+(84, 'HR-Attendance-Single-Report', 'hr-attendance-single-report', 4, NULL, NULL),
+(85, 'HR-Attendance-Advance-Report', 'hr-attendance-advance-report', 4, NULL, NULL),
+(86, 'HR-Attendance-Punch-Data-Syn', 'hr-attendance-punch-data-syn', 4, NULL, NULL),
+(87, 'LM-Attendance-Report', 'lm-attendance-report', 4, NULL, NULL),
+(88, 'LM-Attendance-Create', 'lm-attendance-create', 4, NULL, NULL),
+(89, 'Concern-Attendance-Report', 'concern-attendance-report', 4, NULL, NULL),
+(90, 'Concern-Attendance-Create', 'concern-attendance-create', 4, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_permission_module`
+--
+
+CREATE TABLE `tbl_permission_module` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_permission_module`
+--
+
+INSERT INTO `tbl_permission_module` (`id`, `name`) VALUES
+(1, 'Role & Permission '),
+(2, 'Leave Module'),
+(3, 'Tour Module'),
+(4, 'Attendance  Module'),
+(5, 'PMS Module'),
+(7, 'Roster Module');
 
 -- --------------------------------------------------------
 
@@ -148,96 +205,6 @@ CREATE TABLE `tbl_roles_permissions` (
   `role_id` bigint(20) UNSIGNED NOT NULL,
   `permission_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `tbl_roles_permissions`
---
-
-INSERT INTO `tbl_roles_permissions` (`id`, `role_id`, `permission_id`) VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3),
-(4, 1, 4),
-(5, 1, 5),
-(6, 1, 6),
-(7, 1, 7),
-(8, 1, 8),
-(9, 1, 9),
-(10, 1, 10),
-(11, 1, 11),
-(12, 1, 12),
-(13, 1, 13),
-(17, 1, 14),
-(19, 1, 15),
-(20, 1, 16),
-(21, 1, 17),
-(22, 1, 18),
-(23, 1, 19),
-(24, 1, 20),
-(25, 1, 21),
-(26, 1, 22),
-(27, 1, 23),
-(28, 1, 24),
-(29, 1, 25),
-(30, 1, 26),
-(31, 1, 27),
-(32, 1, 28),
-(33, 1, 29),
-(34, 1, 30),
-(35, 1, 31),
-(36, 1, 32),
-(37, 1, 33),
-(38, 1, 34),
-(39, 1, 35),
-(40, 1, 36),
-(41, 1, 37),
-(42, 1, 38),
-(43, 1, 39),
-(44, 1, 40),
-(45, 1, 41),
-(46, 1, 42),
-(47, 1, 43),
-(48, 1, 44),
-(49, 1, 45),
-(50, 1, 46),
-(51, 7, 17),
-(52, 7, 18),
-(53, 7, 19),
-(54, 7, 20),
-(55, 7, 21),
-(56, 7, 22),
-(57, 7, 23),
-(58, 7, 24),
-(59, 7, 25),
-(60, 7, 26),
-(61, 7, 27),
-(62, 7, 28),
-(63, 7, 29),
-(64, 7, 30),
-(65, 7, 31),
-(66, 7, 32),
-(67, 7, 33),
-(68, 7, 34),
-(69, 7, 35),
-(70, 7, 36),
-(71, 7, 37),
-(72, 7, 38),
-(73, 7, 39),
-(74, 7, 40),
-(78, 7, 42),
-(79, 7, 41),
-(82, 5, 6),
-(83, 5, 13),
-(84, 2, 1),
-(85, 2, 2),
-(86, 3, 1),
-(87, 3, 2),
-(88, 6, 45),
-(89, 4, 1),
-(90, 4, 2),
-(91, 4, 35),
-(92, 4, 36),
-(93, 4, 45);
 
 -- --------------------------------------------------------
 
@@ -306,8 +273,8 @@ INSERT INTO `tbl_users` (`id`, `user_role_id`, `first_name`, `emp_id`, `concern`
 (48, 4, 'Hasan Mahamud', 'RML-00713', 'RML', 'dd@gmail.com', '07C5807D0D927DCD0980F86024E5208B', '713', NULL),
 (49, 4, 'Avijit Roy', 'RML-01115', 'RML', 'webuser@mail.com', 'e10adc3949ba59abbe56e057f20f883e', '123456', NULL),
 (50, 3, 'Ahmed Shahriar Anwar', 'RML-01248', 'RML', 'webuser@mail.com', 'e10adc3949ba59abbe56e057f20f883e', '123456', NULL),
-(51, 3, 'Md. Zahirul Islam', 'RMWL-0887', 'RML', 'webuser@mail.com', 'e10adc3949ba59abbe56e057f20f883e', '123456', NULL),
-(52, 4, 'Joy Paul g', 'RML-01260', 'RML', 'joy.paul@rangsgroup.com', '0192023a7bbd73250516f069df18b500', 'admin123', 'uploads/50_1686633365.jpg');
+(51, 4, 'Md. Zahirul Islam', 'RMWL-0887', 'RML', 'webuser@mail.com', '0192023a7bbd73250516f069df18b500', '123456', NULL),
+(52, 4, 'Joy Paul', 'RML-01260', 'RML', 'joy.paul@rangsgroup.com', '0192023a7bbd73250516f069df18b500', 'admin123', 'uploads/50_1686633365.jpg');
 
 -- --------------------------------------------------------
 
@@ -327,6 +294,7 @@ CREATE TABLE `tbl_users_permissions` (
 --
 
 CREATE TABLE `tbl_users_roles` (
+  `id` int(10) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `role_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -335,10 +303,12 @@ CREATE TABLE `tbl_users_roles` (
 -- Dumping data for table `tbl_users_roles`
 --
 
-INSERT INTO `tbl_users_roles` (`user_id`, `role_id`) VALUES
-(52, 1),
-(1, 1),
-(52, 2);
+INSERT INTO `tbl_users_roles` (`id`, `user_id`, `role_id`) VALUES
+(1, 1, 1),
+(2, 2, 7),
+(4, 1, 7),
+(6, 52, 7),
+(7, 52, 1);
 
 -- --------------------------------------------------------
 
@@ -378,6 +348,13 @@ ALTER TABLE `tbl_password_history`
 -- Indexes for table `tbl_permissions`
 --
 ALTER TABLE `tbl_permissions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `permission_id` (`permission_module_id`);
+
+--
+-- Indexes for table `tbl_permission_module`
+--
+ALTER TABLE `tbl_permission_module`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -412,6 +389,7 @@ ALTER TABLE `tbl_users_permissions`
 -- Indexes for table `tbl_users_roles`
 --
 ALTER TABLE `tbl_users_roles`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `users_roles_ibfk_1` (`role_id`);
 
@@ -435,7 +413,13 @@ ALTER TABLE `tbl_password_history`
 -- AUTO_INCREMENT for table `tbl_permissions`
 --
 ALTER TABLE `tbl_permissions`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
+
+--
+-- AUTO_INCREMENT for table `tbl_permission_module`
+--
+ALTER TABLE `tbl_permission_module`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_roles`
@@ -447,13 +431,19 @@ ALTER TABLE `tbl_roles`
 -- AUTO_INCREMENT for table `tbl_roles_permissions`
 --
 ALTER TABLE `tbl_roles_permissions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `tbl_users_roles`
+--
+ALTER TABLE `tbl_users_roles`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_user_role`
