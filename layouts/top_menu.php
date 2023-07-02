2b8e47e578a9f76e9dadbf2a3ed39ba0286944a8
joypaul1 <joypaul.bd1997@gmail.com>
2023-07-02 13:02:48 +0600

    image path

--- layouts/top_menu.php
+++ layouts/top_menu.php
@@ -53,7 +53,7 @@
           <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
             <div class="avatar avatar-online">
               <img
-                src="<?php echo $_SESSION['HR']['emp_image_hr'] != null ?($basePath.'/'.$_SESSION['HR']['emp_image_hr']) : "/assets/img/avatars/1.png"; ?>"
+                src="<?php echo $_SESSION['HR']['emp_image_hr'] != null ?($basePath.'/'.$_SESSION['HR']['emp_image_hr']) : $baseUrl.'/'."assets/img/avatars/1.png"; ?>"
                 alt class="w-px-40 h-auto rounded-circle" />
             </div>
           </a>
@@ -64,7 +64,7 @@
                   <div class="flex-shrink-0 me-3">
                     <div class="avatar avatar-online">
                       <img
-                        src="<?php echo $_SESSION['HR']['emp_image_hr'] != null ?($baseUrl.'/'.$_SESSION['HR']['emp_image_hr']) : "assets/img/avatars/1.png"; ?>"
+                        src="<?php echo $_SESSION['HR']['emp_image_hr'] != null ?($basePath.'/'.$_SESSION['HR']['emp_image_hr']) : $basePath.'/'."assets/img/avatars/1.png"; ?>"
                         alt class="w-px-40 h-auto rounded-circle" />
                     </div>
                   </div>
