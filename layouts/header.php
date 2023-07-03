0bb057bfd917f76d026b0e8e10fb225212c80cf6
joypaul1 <joypaul.bd1997@gmail.com>
2023-07-03 15:30:28 +0600

    Favicon

--- layouts/header.php
+++ layouts/header.php
@@ -5,51 +5,51 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Mobile Apps Admin Panel</title>
+  <title>Rangs Mobile Apps Admin Panel</title>
 
   <meta name="description" content="" />
   <?php
-    $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
-    $basePath =  $baseUrl.'/rHRT';
-    if(!isset($_SESSION['HR'])){
-      header("location:" . $basePath . "/index.php");
-      exit();
-    }
+  $baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
+  $basePath =  $baseUrl . '/rHRT';
+  if (!isset($_SESSION['HR'])) {
+    header("location:" . $basePath . "/index.php");
+    exit();
+  }
   ?>
 
 
   <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="assets/img/favicon/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="<?php echo $basePath ?>/assets/img/favicon/favicon.ico" />
 
   <!-- Fonts -->
 
   <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
   <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/vendor/fonts/boxicons.css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/fonts/boxicons.css" />
 
   <!-- Core CSS -->
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/css/demo.css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/core.css" class="template-customizer-core-css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/css/demo.css" />
 
   <!-- Vendors CSS -->
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-  <link rel="stylesheet" href="<?php  echo $basePath ?>/assets/vendor/libs/apex-charts/apex-charts.css" />
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/apex-charts/apex-charts.css" />
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-  
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.8/sweetalert2.min.css" integrity="sha512-y4S4cBeErz9ykN3iwUC4kmP/Ca+zd8n8FDzlVbq5Nr73gn1VBXZhpriQ7avR+8fQLpyq4izWm0b8s6q4Vedb9w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <!-- Page CSS -->
 
   <!-- Helpers -->
-  <script src="<?php  echo $basePath ?>/assets/vendor/js/helpers.js"></script>
+  <script src="<?php echo $basePath ?>/assets/vendor/js/helpers.js"></script>
 
   <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
   <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-  <script src="<?php  echo $basePath ?>/assets/js/config.js"></script>
- 
+  <script src="<?php echo $basePath ?>/assets/js/config.js"></script>
+
 
 
 </head>
