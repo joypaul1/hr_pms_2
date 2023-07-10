e62ba161bb78c901aaca334211233e5ce1956cf7
joypaul1 <joypaul.bd1997@gmail.com>
2023-07-10 15:56:21 +0600

    ok

--- pms/view/pms_kpi_list.php
+++ pms/view/pms_kpi_list.php
@@ -1,12 +1,12 @@
 <?php
-
-
 require_once('../../helper/2step_com_conn.php');
 require_once('../../inc/connoracle.php');
+require_once('../../../helper/3step_com_conn.php');
+require_once('../../../inc/connoracle.php');
 
-
-
-
+if (!checkPermission('concern-clearence-create')) {
+    echo "<script> window.location.href = '$basePath/index.php?logout=true'; </script>";
+}
 ?>
 
 
