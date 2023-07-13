6c02b65bdff47852ad3472d56f0cf8b89faa4be6
Firoz-ocp <firoz.ocp@gmail.com>
2023-07-13 12:41:12 +0600

    update

--- clearence_module/view/lm_panel/approval.php
+++ clearence_module/view/lm_panel/approval.php
@@ -8,10 +8,8 @@ if (!checkPermission('lm-clearence-approval')) {
     echo "<script> window.location.href = '$basePath/index.php?logout=true'; </script>";
 }
 
-
+$emp_session_id = $_SESSION['HR']['emp_id_hr'];
 ?>
-
-
 <!-- / Content -->
 
 <div class="container-xxl flex-grow-1 container-p-y">
@@ -21,48 +19,26 @@ if (!checkPermission('lm-clearence-approval')) {
             <form id="Form2" action="" method="post"></form>
             <form id="Form3" action="" method="post"></form>
             <div class="row">
-                <div class="col-sm-3">
-                    <label>Select Your Concern:</label>
-                    <select name="emp_concern" class="form-control  cust-control" form="Form1">
-                        <option selected value="">---</option>
-                        <?php
-
-
-                        $strSQL  = oci_parse($objConnect, "select RML_ID,EMP_NAME from RML_HR_APPS_USER 
-                                                                            where LINE_MANAGER_RML_ID ='$emp_session_id'
-                                                                            and is_active=1 
-                                                                            order by EMP_NAME");
-                        oci_execute($strSQL);
-                        while ($row = oci_fetch_assoc($strSQL)) {
-                        ?>
-
-                            <option value="<?php echo $row['RML_ID']; ?>"><?php echo $row['EMP_NAME']; ?></option>
-                        <?php
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="col-sm-3">
-                    <label>From Date:</label>
-                    <input required="" class="form-control  cust-control" form="Form1" name="start_date" type="date" />
-
-                </div>
-                <div class="col-sm-3">
-                    <label>To Date:</label>
-                    <input required="" class="form-control  cust-control" form="Form1" id="date" name="end_date" type="date" />
-
+			     <div class="col-sm-6"></div>
+                 <div class="col-sm-3">
+                    <div class="form-group">
+                        <label class="form-label" for="basic-default-fullname">EMP RML ID</label>
+                        <input required="" form="Form1" placeholder="Employee ID" name="emp_concern" class="form-control cust-control" type='text' value='<?php echo isset($_POST['emp_concern']) ? $_POST['emp_concern'] : ''; ?>' />
+                    </div>
                 </div>
                 <div class="col-sm-3">
                     <label>&nbsp;</label>
+					<div class="form-group">
                     <input class="form-control btn btn-sm btn-primary" type="submit" value="Search Approval Data" form="Form1">
+                    </div>
                 </div>
             </div>
 
         </div>
 
         <div class="card  col-lg-12 mt-2">
-            <h5 class="card-header"><b>Concern Leave Taken List</b></h5>
-            <form id="Form1" action="" method="post " class ="card-body">
+            <h5 class="card-header"><b>Clearence Approval List</b></h5>
+            <form id="Form2" action="" method="post " class ="card-body">
                 <div class="">
                     <div class="resume-item d-flex flex-column flex-md-row">
                         <table class="table table-bordered piechart-key" id="admin_list" style="width:100%">
@@ -70,28 +46,37 @@ if (!checkPermission('lm-clearence-approval')) {
                                 <tr class="text-center">
                                     <th scope="col">Sl</th>
                                     <th scope="col">Emp Info</th>
-                                    <th scope="col">Entry Info</th>
                                 </tr>
                             </thead>
 
 
 
                             <?php
-                            @$emp_concern = $_REQUEST['emp_concern'];
-                            @$attn_start_date = date("d/m/Y", strtotime($_REQUEST['start_date']));
-                            @$attn_end_date = date("d/m/Y", strtotime($_REQUEST['end_date']));
-
-                            if (isset($_POST['start_date'])) {
-
-                                $strSQL  = oci_parse($objConnect, "select a.ID,b.EMP_NAME,a.RML_ID,a.ENTRY_DATE,a.START_DATE,a.END_DATE,a.REMARKS,a.ENTRY_BY,b.DEPT_NAME,b.BRANCH_NAME,b.DESIGNATION
-														from RML_HR_EMP_LEAVE a,RML_HR_APPS_USER b
-														where a.RML_ID=b.RML_ID
-														and b.LINE_MANAGER_RML_ID='$emp_session_id'
-														 and ('$emp_concern' is null OR A.RML_ID='$emp_concern')
-														 and (trunc(START_DATE) BETWEEN TO_DATE('$attn_start_date','dd/mm/yyyy') AND TO_DATE('$attn_end_date','dd/mm/yyyy') OR
-                                                              trunc(END_DATE) BETWEEN TO_DATE('$attn_start_date','dd/mm/yyyy') AND TO_DATE('$attn_end_date','dd/mm/yyyy'))
-														and a.LINE_MNGR_APVL_STS IS NULL
-														order by START_DATE desc");
+                           
+                            
+                            if (isset($_POST['emp_concern'])) {
+                                $emp_concern = $_REQUEST['emp_concern'];
+                                $strSQL  = oci_parse($objConnect, "SELECT B.ID,
+																	   C.EMP_NAME,
+																	   C.RML_ID,
+																	   C.R_CONCERN,
+																	   C.DEPT_NAME,
+																	   C.DESIGNATION,
+																	   C.BRANCH_NAME,
+																	   A.CREATED_DATE,
+																	   A.CREATED_BY
+																FROM EMP_CLEARENCE A,EMP_CLEARENCE_DTLS B,RML_HR_APPS_USER C
+																WHERE A.ID=B.EMP_CLEARENCE_ID
+																AND A.RML_HR_APPS_USER_ID=C.ID
+																AND C.RML_ID='$emp_concern'
+																AND B.CONCERN_NAME IN (
+																				SELECT R_CONCERN from HR_DEPT_CLEARENCE_CONCERN WHERE RML_HR_APPS_USER_ID=
+																				(SELECT ID FROM RML_HR_APPS_USER WHERE RML_ID='$emp_session_id')
+																				 )
+																AND B.DEPARTMENT_ID IN (
+																				SELECT DEPARTMENT_ID from HR_DEPT_CLEARENCE_CONCERN WHERE RML_HR_APPS_USER_ID=
+																				(SELECT ID FROM RML_HR_APPS_USER WHERE RML_ID='$emp_session_id')
+																				)");
 
                                 oci_execute($strSQL);
                                 $number = 0;
@@ -102,7 +87,7 @@ if (!checkPermission('lm-clearence-approval')) {
                             ?>
                                     <tbody>
                                         <tr>
-                                            <td><input type="checkbox" name="check_list[]" value="<?php echo $row['ID']; ?>">
+                                            <td><input form="Form2 type="checkbox" name="check_list[]" value="<?php echo $row['ID']; ?>">
                                                 <?php echo $number; ?>
                                             </td>
                                             <td>
@@ -115,19 +100,7 @@ if (!checkPermission('lm-clearence-approval')) {
                                                 echo $row['DESIGNATION'];
                                                 echo ',<br>';
                                                 echo $row['BRANCH_NAME']; ?>
-                                            </td>
-                                            <td>
-                                                <?php echo $row['START_DATE'] . '-to-' . $row['END_DATE'];
-                                                echo ',<br>';
-                                                $v_leave_day = abs($row['END_DATE'] - $row['START_DATE']) + 1;
-                                                echo $v_leave_day;
-                                                if ($v_leave_day > 1)
-                                                    echo '-Days';
-                                                else
-                                                    echo '-Day';
-                                                echo ',<br>';
-                                                echo 'Remarks:-' . $row['REMARKS'];
-                                                ?>
+                                               <!-- <input class="btn btn-primary btn pull-right" type="submit" name="submit_approval_single" value="Approve" /> -->
                                             </td>
                                         </tr>
                                     <?php
@@ -135,7 +108,6 @@ if (!checkPermission('lm-clearence-approval')) {
                                 if ($v_view_approval > 0) {
                                     ?>
                                         <tr>
-                                            <td></td>
                                             <td>
                                                 <input class="btn btn-primary btn pull-right" type="submit" name="submit_approval" value="Approve" />
                                             </td>
@@ -147,13 +119,27 @@ if (!checkPermission('lm-clearence-approval')) {
                                 }
                             } else {
 
-                                $allDataSQL  = oci_parse($objConnect, "select a.ID,b.EMP_NAME,a.RML_ID,a.ENTRY_DATE,a.START_DATE,a.END_DATE,a.REMARKS,a.ENTRY_BY,b.DEPT_NAME,b.BRANCH_NAME,b.DESIGNATION
-														from RML_HR_EMP_LEAVE a,RML_HR_APPS_USER b
-														where a.RML_ID=b.RML_ID
-														and b.LINE_MANAGER_RML_ID='$emp_session_id'
-														and a.LINE_MNGR_APVL_STS IS NULL
-														and trunc(START_DATE)> TO_DATE('01/01/2022','DD/MM/YYYY') 
-														order by START_DATE desc");
+                                $allDataSQL  = oci_parse($objConnect, "SELECT B.ID,
+																	   C.EMP_NAME,
+																	   C.RML_ID,
+																	   C.R_CONCERN,
+																	   C.DEPT_NAME,
+																	   C.DESIGNATION,
+																	   C.BRANCH_NAME,
+																	   A.CREATED_DATE,
+																	   A.CREATED_BY
+																FROM EMP_CLEARENCE A,EMP_CLEARENCE_DTLS B,RML_HR_APPS_USER C
+																WHERE A.ID=B.EMP_CLEARENCE_ID
+																AND A.RML_HR_APPS_USER_ID=C.ID
+																AND B.APPROVAL_STATUS IS NULL
+																AND B.CONCERN_NAME IN (
+																				SELECT R_CONCERN from HR_DEPT_CLEARENCE_CONCERN WHERE RML_HR_APPS_USER_ID=
+																				(SELECT ID FROM RML_HR_APPS_USER WHERE RML_ID='$emp_session_id')
+																				 )
+																AND B.DEPARTMENT_ID IN (
+																				SELECT DEPARTMENT_ID from HR_DEPT_CLEARENCE_CONCERN WHERE RML_HR_APPS_USER_ID=
+																				(SELECT ID FROM RML_HR_APPS_USER WHERE RML_ID='$emp_session_id')
+																				)");
 
                                 @oci_execute($allDataSQL);
                                 $number = 0;
@@ -163,7 +149,7 @@ if (!checkPermission('lm-clearence-approval')) {
                                     $v_view_approval = 1;
                                     ?>
                                         <tr>
-                                            <td><input type="checkbox" name="check_list[]" value="<?php echo $row['ID']; ?>">
+                                            <td><input form="Form2" type="checkbox" name="check_list[]" value="<?php echo $row['ID']; ?>">
                                                 <?php echo $number; ?>
                                             </td>
                                             <td>
@@ -176,43 +162,25 @@ if (!checkPermission('lm-clearence-approval')) {
                                                 echo $row['DESIGNATION'];
                                                 echo ',<br>';
                                                 echo $row['BRANCH_NAME']; ?>
-                                                <input class="btn btn-primary btn pull-right" type="submit" name="submit_approval_single" value="Approve" />
+                                               <!-- <input class="btn btn-primary btn pull-right" type="submit" name="submit_approval_single" value="Approve" /> -->
                                             </td>
-                                            <td>
-                                                <?php echo $row['START_DATE'] . '-to-' . $row['END_DATE'];
-                                                echo ',<br>';
-                                                $v_leave_day = abs($row['END_DATE'] - $row['START_DATE']) + 1;
-                                                echo $v_leave_day;
-                                                if ($v_leave_day > 1)
-                                                    echo '-Days';
-                                                else
-                                                    echo '-Day';
-                                                echo ',<br>';
-                                                echo 'Remarks:-' . $row['REMARKS'];
-                                                ?>
-                                            </td>
-
-
                                         </tr>
                                     <?php
                                 }
                                 if ($v_view_approval > 0) {
                                     ?>
                                         <tr>
-                                            <td></td>
                                             <td>
-                                                <input class="btn btn-primary btn pull-right" type="submit" name="submit_approval" value="Approve" />
+                                                <input form="Form2" class="btn btn-primary btn pull-right" type="submit" name="submit_approval" value="Approve" />
                                             </td>
                                             <td>
                                                 <input class="btn btn-primary btn pull-right" type="submit" name="submit_denied" value="Denied" />
                                             </td>
-
                                         </tr>
                                 <?php
-                                }
-                            }
+                                }}
                                 ?>
-                                    </tbody>
+                            </tbody>
 
                         </table>
                     </div>
@@ -225,26 +193,17 @@ if (!checkPermission('lm-clearence-approval')) {
         if (isset($_POST['submit_approval_single'])) {
             if (!empty($_POST['check_list'])) {
                 foreach ($_POST['check_list'] as $TT_ID_SELECTTED) {
-                    $strSQL = oci_parse(
-                        $objConnect,
-                        "update RML_HR_EMP_LEAVE 
-										set LINE_MNGR_APVL_STS=1,
-										LINE_MNGR_APVL_DATE=sysdate,
-										LINE_MNGR_APVL_BY='$emp_session_id',
-										IS_APPROVED=1
-                                         where ID='$TT_ID_SELECTTED'"
-                    );
-
-                    oci_execute($strSQL);
-                    $attnProcSQL  = oci_parse($objConnect, "declare V_START_DATE VARCHAR2(100);
-						                                                 V_END_DATE VARCHAR2(100);
-																		 V_RML_ID VARCHAR2(100);
-									  begin SELECT TO_CHAR(START_DATE,'dd/mm/yyyy'),TO_CHAR(END_DATE,'dd/mm/yyyy'),RML_ID  INTO V_START_DATE,V_END_DATE,V_RML_ID  FROM RML_HR_EMP_LEAVE WHERE ID=$TT_ID_SELECTTED; RML_HR_ATTN_PROC(V_RML_ID,TO_DATE(V_START_DATE,'dd/mm/yyyy'),TO_DATE(V_END_DATE,'dd/mm/yyyy'));end;");
+               
+                    $attnProcSQL  = oci_parse($objConnect, "UPDATE EMP_CLEARENCE_DTLS
+								SET    APPROVAL_STATUS  = 1,
+									   APPROVE_BY       = '$emp_session_id',
+									   APPROVE_DATE     = SYSDATE
+								WHERE  ID               = '$TT_ID_SELECTTED'");
 
                     if (oci_execute($attnProcSQL)) {
                         //$errorMsg = "Your Selected Leave Successfully Approved";
                         echo '<div class="alert alert-primary">';
-                        echo 'Successfully Approved Outdoor Attendance ID ' . $TT_ID_SELECTTED;
+                        echo 'Successfully Approved Clearence ID ' . $TT_ID_SELECTTED;
                         echo '<br>';
                         echo '</div>';
                     }
@@ -264,31 +223,26 @@ if (!checkPermission('lm-clearence-approval')) {
 
 
         if (isset($_POST['submit_approval'])) { //to run PHP script on submit
+
             if (!empty($_POST['check_list'])) {
                 // Loop to store and display values of individual checked checkbox.
                 foreach ($_POST['check_list'] as $TT_ID_SELECTTED) {
-                    $strSQL = oci_parse(
-                        $objConnect,
-                        "update RML_HR_EMP_LEAVE 
-										set LINE_MNGR_APVL_STS=1,
-										LINE_MNGR_APVL_DATE=sysdate,
-										LINE_MNGR_APVL_BY='$emp_session_id',
-										IS_APPROVED=1
-                                         where ID='$TT_ID_SELECTTED'"
-                    );
-
-                    oci_execute($strSQL);
-                    $attnProcSQL  = oci_parse($objConnect, "declare V_START_DATE VARCHAR2(100); V_END_DATE VARCHAR2(100); V_RML_ID VARCHAR2(100);begin  SELECT TO_CHAR(START_DATE,'dd/mm/yyyy'),TO_CHAR(END_DATE,'dd/mm/yyyy'),RML_ID INTO V_START_DATE,V_END_DATE,V_RML_ID  FROM RML_HR_EMP_LEAVE WHERE ID=$TT_ID_SELECTTED; RML_HR_ATTN_PROC(V_RML_ID,TO_DATE(V_START_DATE,'dd/mm/yyyy'),TO_DATE(V_END_DATE,'dd/mm/yyyy'));end;");
+                   
+                    $attnProcSQL  = oci_parse($objConnect, "UPDATE EMP_CLEARENCE_DTLS
+								SET    APPROVAL_STATUS  = 1,
+									   APPROVE_BY       = '$emp_session_id',
+									   APPROVE_DATE     = SYSDATE
+								WHERE  ID               = $TT_ID_SELECTTED");
 
                     if (oci_execute($attnProcSQL)) {
                         //$errorMsg = "Your Selected Leave Successfully Approved";
                         echo '<div class="alert alert-primary">';
-                        echo 'Successfully Approved Outdoor Attendance ID ' . $TT_ID_SELECTTED;
+                        echo 'Successfully Approved Clearence ID ' . $TT_ID_SELECTTED;
                         echo '<br>';
                         echo '</div>';
                     }
                 }
-                echo "<script>window.location = 'http://202.40.181.98:9090/rHR/lm_leave_approval.php'</script>";
+                echo "<script>window.location = '$basePath/clearence_module/view/lm_panel/approval.php'</script>";
             } else {
                 //$errorMsg = "Sorry! You have not select any ID Code.";
 
@@ -296,6 +250,7 @@ if (!checkPermission('lm-clearence-approval')) {
                 echo 'Sorry! You have not select any ID Code.';
                 echo '</div>';
             }
+				
         }
 
         // Denied option
