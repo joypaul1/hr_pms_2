61619b07948b2485fe0c91ef2a77f9bdf9379ab7
joypaul1 <jyla5102555@gmail.com>
2023-08-13 15:38:07 +0600

    ok

--- form_module/view/car_deed_form.php
+++ form_module/view/car_deed_form.php
@@ -19,7 +19,7 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
 
     <div class="card col-lg-12">
         <form action="" method="post">
-            <input type="hidden" name="actionType" value="searchData" >
+            <input type="hidden" name="actionType" value="searchData">
             <div class="card-body row">
                 <div class="col-sm-3"></div>
                 <div class="col-sm-4">
@@ -40,72 +40,130 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
     </div>
 
     <!-- Bordered Table -->
-    <div class="card mt-2">
-
+    <div class="card  mt-1">
+        <form action="<?php echo $basePath . '/form_module/view/car_deed_print_form.php' ?>" method="POST" >
+        <input type="text" name="invoice_number" value="<?php echo isset($_POST["invoice_id"]) ? trim($_POST["invoice_id"]) : ' ' ?>">
+        <input type="hidden" name="actionType" value="car_deed">
         <div class="card-body row">
-            <div class="col-8">
-                <div style="
-                        border: 1px solid #eee5e5;
-                        padding: 2%;
-                        box-shadow: 1px 1px 1px 1px lightgray;
-                        margin: 2%;
-                    ">
-                    <h5 class="text-center"> <i class="menu-icon tf-icons bx bx-right-arrow m-0 text-info"></i>Invoice Number :
-                     <?php echo isset($_POST["invoice_id"]) ? trim($_POST["invoice_id"]) : ' ' ?> </h5>
-                    <p class="text-center"> <i class=" m-0 text-info"></i><u>Car Reference List</u> </p>
+                <div class="col-md-7 ">
+                    <div style="
+                    border: 1px solid #eee5e5;
+                    padding: 1%;
+                    margin: 1%;
+                ">
+                        <h5 class="text-center"> <i class="menu-icon tf-icons bx bx-right-arrow m-0 text-info"></i>Invoice Number :
+                            <?php echo isset($_POST["invoice_id"]) ? trim($_POST["invoice_id"]) : ' ' ?> </h5>
+                        <p class="text-center"> <i class=" m-0 text-info"></i><u>Car Reference List</u> </p>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>REF. CODE</th>
+                                    <th>Cus. Name</th>
+                                    <th>Cus. Mobile</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                if ($_SERVER['REQUEST_METHOD'] === 'POST' && trim($_POST["actionType"])  == 'searchData') {
+                                    $invoice_id = trim($_POST["invoice_id"]);
 
-                    <?php 
-                    if ($_SERVER['REQUEST_METHOD'] === 'POST' && trim($_POST["actionType"])  == 'searchData') { 
-                        $invoice_id = trim($_POST["invoice_id"]);
+                                    $deedSQL = oci_parse($objConnect, "SELECT REF_CODE,CUSTOMER_NAME,CUSTOMER_MOBILE_NO  FROM LEASE_ALL_INFO_ERP WHERE DOCNUMBR = :invoice_id");
+                                    oci_bind_by_name($deedSQL, ":invoice_id", $invoice_id);
+                                    oci_execute($deedSQL);
+                                    // REF_CODE != '' AND 
+                                    if ($row = oci_fetch_assoc($deedSQL)) {
+                                        do {
+                                            echo '<tr>
+                                        <td><input type="checkbox" class="form-check-input" value="' . $row['REF_CODE'] . '" name="reference_id[]" id="' . $row['REF_CODE'] . '">
+                                        <label class="form-check-label" for="' . $row['REF_CODE'] . '">' . $row['REF_CODE'] . '</label></td>
+                                        <td>
+                                        <label class="form-check-label" for="' . $row['REF_CODE'] . '"> ' . $row['CUSTOMER_NAME'] . '</label>
+                                        </td>
+                                        <td>
+                                        <label class="form-check-label" for="' . $row['REF_CODE'] . '"> ' . $row['CUSTOMER_MOBILE_NO'] . '</label>
+                                        </td>
+                                    </tr>';
+                                        } while ($row = oci_fetch_assoc($deedSQL));
+                                    } else {
+                                        echo '<strong class="text-danger">Sorry! No data found.</strong>';
+                                    }
+                                }
+                                ?>
+                            </tbody>
+                        </table>
 
-                        $deedSQL = oci_parse($objConnect, "SELECT REF_CODE FROM LEASE_ALL_INFO_ERP WHERE DOCNUMBR = :invoice_id");
-                        oci_bind_by_name($deedSQL, ":invoice_id", $invoice_id);
-                        oci_execute($deedSQL);
-                        
-                        if ($row = oci_fetch_assoc($deedSQL)) {
-                            do {
-                                echo '<div class="form-check-inline col-5">
-                                        <input type="checkbox" class="form-check-input" value="'.$row['REF_CODE'].'" name="reference_id[]" id="'.$row['REF_CODE'].'">
-                                        <label class="form-check-label" for="'.$row['REF_CODE'].'">'.$row['REF_CODE'].'</label>
-                                    </div>';
-                            } while ($row = oci_fetch_assoc($deedSQL));
-                        } else {
-                            echo '<strong class="text-danger">Sorry! No data found. </strong>';
-                        }
-                        
-                    }
-                    ?>
-                 
-                       
 
-                </div>
-            </div>
-            <div class="col-4">
-                <div style="
-                        border: 1px solid #eee5e5;
-                        padding: 2%;
-                        box-shadow: 1px 1px 1px 1px lightgray;
-                        margin: 2%;
-                    ">
-                     <div class="form-inline col-12">
-                         <label class="d-block form-label text-center " for="cheque_number"><b>cheque Number</b></label>
-                        <input type="text" class="form-control" placeholder="cheque Number" name="cheque_number" id="cheque_number">
-                    </div>
-                     <div class="form-inline col-12">
-                       
-                     <!-- <label class="d-block form-label text-center "for="submit">&nbsp;label> -->
 
-                     <!-- <input class="form-control btn btn-sm btn-primary" type="submit" value="Search Data"> -->
                     </div>
                 </div>
 
+                <div class="col-md-5">
+                    <div style="border: 1px solid #eee5e5;padding: 2%; margin: 1%">
+                        <div class="form-group">
+                            <label for="cheque_number"> Cheque Number</label>
+                            <input type="text" class="form-control" name="cheque_number" id="cheque_number" placeholder="Cheque Number">
+                        </div>
+                        <div class="form-group">
+                            <label for="cheque_number"> Invoice Date</label>
+                            <input type="date" class="form-control" name="date" id="date">
+                        </div>
+                        <div class="form-group">
+                            <label for="c_f_name">Customer Father's Name</label>
+                            <input type="text" class="form-control" name="c_f_name" id="c_f_name" placeholder="Customer father's Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_name_1"> Guarantor/Dealer Name (1)</label>
+                            <input type="text" class="form-control" name="g_name_1" id="g_name_1" placeholder="Guarantor/Dealer Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_f_name_1"> Guarantor Father's Name (1)</label>
+                            <input type="text" class="form-control" name="g_f_name_1" id="g_f_name_1" placeholder="Guarantor/Dealer Father's Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_add_1"> Guarantor Address (1)</label>
+                            <input type="text" class="form-control" name="g_add_1" id="g_add_1" placeholder="Guarantor/Dealer Address">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_name_2"> Guarantor/Dealer Name (2)</label>
+                            <input type="text" class="form-control" name="g_name_2" id="g_name_2" placeholder="Guarantor/Dealer Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_f_name_2"> Guarantor/Dealer Father's Name (2)</label>
+                            <input type="text" class="form-control" name="g_f_name_2" id="g_f_name_2" placeholder="Guarantor/Dealer Father's Name">
+                        </div>
+                        <div class="form-group">
+                            <label for="g_add_2"> Guarantor/Dealer Address (2)</label>
+                            <input type="text" class="form-control" name="g_add_2" id="g_add_2" placeholder="Guarantor/Dealer Address">
+                        </div>
+                        <div class="form-group">
+                            <label for="product_model"> Product Model</label>
+                            <input type="text" class="form-control" name="product_model" id="product_model" placeholder="product model(EX:AB-000)">
+                        </div>
+                        <div class="form-group">
+                            <label for="sales_amount"> Sales Amount</label>
+                            <input type="text" class="form-control" name="sales_amount" id="sales_amount" placeholder="EX:10,00,000.00">
+                        </div>
+                        <div class="form-group">
+                            <label for="down_payment"> Down Payment</label>
+                            <input type="text" class="form-control" name="down_payment" id="down_payment" placeholder="EX:5,00,000.00">
+                        </div>
+                        <div class="form-group">
+                            <label for="emi_count"> EMI(instalments) Count</label>
+                            <input type="text" class="form-control" name="emi_count" id="emi_count" placeholder="EX:3/4/5">
+                        </div>
+
+
+
+                    </div>
+
+                </div>
+                <div class="d-flex justify-content-center">
+                    <div class="col-3">
+                        <button type="submit" class="form-control btn btn-sm btn-primary">Submit </button>
+                    </div>
+                </div>
             </div>
-            <div class="d-flex justify-content-center">
-            <div class="col-3">
-            <button type="submit" class="form-control btn btn-sm btn-primary">Submit </button>
-            </div>
-            </div>
-        </div>
+        </form>
     </div>
     <!--/ Bordered Table -->
 
