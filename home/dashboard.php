57cf7a6b1c2fcd92702f92272a2820f38d21ac02
joypaul1 <jyla5102555@gmail.com>
2023-08-16 11:39:03 +0600

    design

--- home/dashboard.php
+++ home/dashboard.php
@@ -17,12 +17,12 @@ $basePath =  $_SESSION['basePath'];
 				<div class="d-flex align-items-end row">
 					<div class="col-sm-7">
 						<div class="card-body">
-							<h5 class="card-title text-primary">Congratulations <?php echo $_SESSION['HR']['first_name_hr']; ?>! 🎉</h5>
-							<p class="mb-4">
+							<h5 class="card-title text-primary">Hi <?php echo $_SESSION['HR']['first_name_hr']; ?>! 🎉</h5>
+							<p class="mb-4s">
 								Access Are Predefine according to <span class="fw-bold">Rangs Motors HR Policy.</span>
 								If you need more access please contact with HR.
 							</p>
-							<a href="" class="btn btn-sm btn-primary">Universal Notification</a>
+							<!-- <a href="" class="btn btn-sm btn-primary">Universal Notification</a> -->
 						</div>
 					</div>
 					<div class="col-sm-5 text-center text-sm-left">
@@ -32,73 +32,7 @@ $basePath =  $_SESSION['basePath'];
 					</div>
 				</div>
 			</div>
-		</div>
-
-		<!--  
-        <div class="col-lg-6 mb-4 order-0">
-            <div class="card">
-			   <div class="">
-                <img src="<?php echo $basePath ?>/images/dashing_images.png" class="img-fluid" style="height:242px;width: 100%;border-radius: 2%;">
-               </div>
-            </div>
-        </div>
-		
-		
-		 <-- Approval -->
-		<div class="col-lg-6 mb-2 order-0">
-			<div class="card">
-				<div class="card-body">
-					<h5 class="card-title text-primary">My Last 10 Days Attendance.</h5>
-					<div class="table-responsive text-nowrap">
-						<table class="table table-bordered">
-							<thead class="">
-								<tr>
-									<th scope="col" align="center"><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>SL</strong></th>
-									<th scope="col" align="center"><strong>Date</strong></th>
-									<th scope="col" align="center"><strong>In-Time</strong></th>
-									<th scope="col" align="center"><strong>Out-Time</strong></th>
-									<th scope="col" align="center"><strong>Status</strong></th>
-								</tr>
-							</thead>
-							<tbody>
-								<?php
-								$allDataSQL  = oci_parse(
-									$objConnect,
-									"select ATTN_DATE,IN_TIME,OUT_TIME,STATUS,DAY_NAME
-                                                                     from RML_HR_ATTN_DAILY_PROC
-                                                                     where trunc(ATTN_DATE) between to_date(sysdate-10,'dd/mm/RRRR') and to_date(sysdate,'dd/mm/RRRR')
-																	and RML_ID='$emp_session_id'
-                                                                    order by ATTN_DATE"
-								);
-								oci_execute($allDataSQL);
-								$number = 0;
-								while ($row = oci_fetch_assoc($allDataSQL)) {
-									$number++;
-								?>
-									<tr>
-										<td align="center"><i class="fab fa-angular fa-lg text-danger me-3"></i>
-											<strong><?php echo $number; ?></strong>
-										</td>
-										<td><?php echo $row['ATTN_DATE']; ?></td>
-										<td><?php echo $row['IN_TIME']; ?></td>
-										<td><?php echo $row['OUT_TIME']; ?></td>
-										<td><?php echo $row['STATUS']; ?></td>
-										
-									</tr>
-								<?php
-								}
-								?>
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- End Approval -->
-		<!--  Approval --> 
-		<div class="col-lg-6 mb-2 order-0">
-			<div class="card">
+			<div class="card mt-1">
 				<div class="card-body">
 					<h5 class="card-title text-primary">Approval Pending List</h5>
 					<div class="table-responsive text-nowrap">
@@ -185,6 +119,73 @@ $basePath =  $_SESSION['basePath'];
 					</div>
 				</div>
 			</div>
+		</div>
+
+		<!--  
+        <div class="col-lg-6 mb-4 order-0">
+            <div class="card">
+			   <div class="">
+                <img src="<?php echo $basePath ?>/images/dashing_images.png" class="img-fluid" style="height:242px;width: 100%;border-radius: 2%;">
+               </div>
+            </div>
+        </div>
+		
+		
+		 <-- Approval -->
+		<div class="col-lg-6 mb-2 order-0">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title text-primary">My Last 10 Days Attendance.</h5>
+					<div class="table-responsive text-nowrap">
+						<table class="table table-bordered">
+							<thead class="">
+								<tr>
+									<th scope="col" align="center"><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>SL</strong></th>
+									<th scope="col" align="center"><strong>Date</strong></th>
+									<th scope="col" align="center"><strong>In-Time</strong></th>
+									<th scope="col" align="center"><strong>Out-Time</strong></th>
+									<th scope="col" align="center"><strong>Status</strong></th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								$allDataSQL  = oci_parse(
+									$objConnect,
+									"select ATTN_DATE,IN_TIME,OUT_TIME,STATUS,DAY_NAME
+                                                                     from RML_HR_ATTN_DAILY_PROC
+                                                                     where trunc(ATTN_DATE) between to_date(sysdate-10,'dd/mm/RRRR') and to_date(sysdate,'dd/mm/RRRR')
+																	and RML_ID='$emp_session_id'
+                                                                    order by ATTN_DATE"
+								);
+								oci_execute($allDataSQL);
+								$number = 0;
+								while ($row = oci_fetch_assoc($allDataSQL)) {
+									$number++;
+								?>
+									<tr>
+										<td align="center"><i class="fab fa-angular fa-lg text-danger me-3"></i>
+											<strong><?php echo $number; ?></strong>
+										</td>
+										<td><?php echo $row['ATTN_DATE']; ?></td>
+										<td><?php echo $row['IN_TIME']; ?></td>
+										<td><?php echo $row['OUT_TIME']; ?></td>
+										<td><?php echo $row['STATUS']; ?></td>
+										
+									</tr>
+								<?php
+								}
+								?>
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!-- End Approval -->
+		<!--  Approval --> 
+		<div class="col-lg-6 mb-2 order-0">
+			
 		</div>
 		<!-- End Approval -->
 
