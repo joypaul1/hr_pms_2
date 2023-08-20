ee9a31a6f8fc3ea6ccf4949d2a88396eed67a4ee
joypaul1 <jyla5102555@gmail.com>
2023-08-20 10:31:07 +0600

    vendor

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'dvries/number-to-words',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0a5b88734ac3416019159764faba40f7e3bbb856',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'dvries/number-to-words' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0a5b88734ac3416019159764faba40f7e3bbb856',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'kwn/number-to-words' => array(
+            'pretty_version' => '2.6.4',
+            'version' => '2.6.4.0',
+            'reference' => 'c4c14c84374baaeef35c47373c500ec7aa28b1b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kwn/number-to-words',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
