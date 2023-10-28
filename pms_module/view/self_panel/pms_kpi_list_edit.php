3252ea20f65dfde7ac8dc886bd7b232c909ebcbf
joypaul1 <jyla5102555@gmail.com>
2023-10-28 14:22:03 +0600

    ddd

--- pms_module/view/self_panel/pms_kpi_list_edit.php
+++ pms_module/view/self_panel/pms_kpi_list_edit.php
@@ -2,9 +2,7 @@
 require_once('../../../helper/3step_com_conn.php');
 require_once('../../../inc/connoracle.php');
 $basePath =  $_SESSION['basePath'];
-// if (!checkPermission('concern-offboarding-create')) {
-//     echo "<script> window.location.href = '$basePath/index.php?logout=true'; </script>";
-// }
+
 $v_ACHIEVEMENT_LOCK_STATUS = 0;
 $v_key = $_REQUEST['id'];
 $emp_session_id = $_SESSION['HR']['emp_id_hr'];
