ffc52240efaabfc8b70903df534ab0335a80b8b0
joypaul1 <jyla5102555@gmail.com>
2023-12-03 13:19:17 +0600

    ok

--- resale_module/view/dashboard_panel/resaleDashboard.php
+++ resale_module/view/dashboard_panel/resaleDashboard.php
@@ -18,7 +18,39 @@ $basePath = $_SESSION['basePath'];
 				<div class="card-body">
 					<h5 class="card-title text-primary">Approval Pending List</h5>
 					<div class="table-responsive text-nowrap">
+						<div class="table-responsive text-nowrap">
+						<table class="table  table-bordered">
+							<thead class="table-dark">
+								<tr>
+									<th scope="col" align="center"><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>SL</strong></th>
+									<th scope="col" align="center"><strong>Approval Type</strong></th>
+									<th scope="col" align="center"><strong>Count</strong></th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
 
+								$number = 0;
+								while ($row = oci_fetch_assoc($allDataSQL)) {
+									$number++;
+								?>
+									<tr>
+										<td align="center"><i class="fab fa-angular fa-lg text-danger me-3 "></i>
+											<strong><?php echo $number; ?></strong>
+										</td>
+										<td><?php echo $row['APPROVAL_TYPE']; ?></td>
+										<td align="center">
+											<a target="_blank" href=<?php echo $row['APPROVAL_LINK']; ?>>
+												<span class="badge badge-center rounded-pill bg-info"><?php echo $row['NUMBER_TOTAL']; ?></span>
+											</a>
+										</td>
+									</tr>
+								<?php
+								}
+								?>
+							</tbody>
+						</table>
+						</div>
 					</div>
 				</div>
 			</div>
