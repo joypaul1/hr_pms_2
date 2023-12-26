2011630a552d8f4cf30de0d465437eae5d735097
Firoz-ocp <firoz.ocp@gmail.com>
2023-12-26 12:53:27 +0600

    update leave index

--- leave_module/view/hr_panel/index.php
+++ leave_module/view/hr_panel/index.php
@@ -17,41 +17,56 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
     <div class="card col-lg-12">
         <form action="" method="post">
             <div class="card-body row justify-content-center">
-                <!-- <div class="col-sm-2"></div> -->
+			<div class="row">
                 <div class="col-sm-3">
                     <div class="form-group">
                         <label class="form-label" for="basic-default-fullname">EMP RML ID</label>
-                        <input required="" placeholder="Employee ID" name="emp_id" class="form-control cust-control" type='text' value='<?php echo isset($_POST['emp_id']) ? $_POST['emp_id'] : ''; ?>' >
+                        <input  placeholder="Employee ID" name="emp_id" class="form-control" type='text' value='<?php echo isset($_POST['emp_id']) ? $_POST['emp_id'] : ''; ?>' >
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <label class="form-label" for="basic-default-fullname">Select Start Date*</label>
+                    <label class="form-label" for="basic-default-fullname">Leave Start Date*</label>
                     <div class="input-group">
                         <div class="input-group-addon">
                             <i class="fa fa-calendar">
                             </i>
                         </div>
-                        <input required="" type="date" name="start_date" class="form-control  cust-control" id="title" value="">
+						<input  required="" class="form-control"  type='date' name='start_date' value='<?php echo isset($_POST['start_date']) ? $_POST['start_date'] : ''; ?>' />
                     </div>
                 </div>
                 <div class="col-sm-3">
-                    <label class="form-label" for="basic-default-fullname">Select End Date*</label>
+                    <label class="form-label" for="basic-default-fullname">Leave End Date*</label>
                     <div class="input-group">
                         <div class="input-group-addon">
                             <i class="fa fa-calendar">
                             </i>
                         </div>
-                        <input required="" type="date" name="end_date" class="form-control  cust-control" id="title" value="">
+						<input  required="" class="form-control"  type='date' name='end_date' value='<?php echo isset($_POST['end_date']) ? $_POST['end_date'] : ''; ?>' />
                     </div>
                 </div>
-
-                <div class="col-sm-2">
+                <div class="col-sm-3">
+					<label class="form-label" for="basic-default-fullname">Select Leave Status</label>
+					<div class="input-group">
+					<select name="leave_status" class="form-control">
+						<option  value="Pending">Pending</option>
+						<option  value="1">Approved</option>
+						<option  value="0">Denied</option>
+					</select>
+					</div>
+				</div>
+               
+            </div>
+			
+			 <div class="row">
+			 <div class="col-sm-9"> </div>
+			    <div class="col-sm-3">
                     <div class="form-group">
                         <label class="form-label" for="basic-default-fullname">&nbsp;</label>
                         <input class="form-control btn btn-sm btn-primary" type="submit" value="Search Data">
                     </div>
                 </div>
-            </div>
+			 </div>
+			 </div>
         </form>
     </div>
   
@@ -61,18 +76,19 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
         <h5 class="card-header"><i class="menu-icon tf-icons bx bx-list-ul" style="margin:0;font-size:30px"></i><b>Leave Taken List</b></h5>
         <div class="card-body">
             <div class="table-responsive text-nowrap">
-                <table class="table table-bordered">
+                <table class="table table-bordered" id="table">
                     <thead class="table-dark">
                         <tr class="text-center">
                             <th>SL</th>
                             <th scope="col">Emp ID</th>
                             <th scope="col">Name</th>
                             <th scope="col">Dept.</th>
+							<th scope="col">Branch</th>
+							<th scope="col">Concern</th>
                             <th scope="col">Leave Type</th>
-                            <th scope="col">To Date</th>
-                            <th scope="col">From Date</th>
+                            <th scope="col">Start Date</th>
+                            <th scope="col">End Date</th>
                             <th scope="col">Entry From</th>
-                            <th scope="col">Branch</th>
                             <th scope="col">Approval Status</th>
                         </tr>
                     </thead>
@@ -82,10 +98,11 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
                         if (isset($_POST['emp_id'])) {
 
                             $v_emp_id = $_REQUEST['emp_id'];
-
-                            $strSQL  = oci_parse(
-                                $objConnect,
-                                "SELECT B.RML_ID,
+                             $v_leave_status = $_REQUEST['leave_status'];
+							 $leave_start_date = date("d/m/Y", strtotime($_REQUEST['start_date']));
+							 $leave_end_date= date("d/m/Y", strtotime($_REQUEST['end_date']));
+                            if($v_leave_status=="Pending"){
+								$quary="SELECT B.RML_ID,
 								B.EMP_NAME,
 								B.DEPT_NAME,
 								B.BRANCH_NAME,
@@ -93,12 +110,41 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
 								A.START_DATE,
 								A.END_DATE,
 								A.ENTRY_FROM,
-								A.IS_APPROVED
+								A.IS_APPROVED,
+								B.R_CONCERN
 							FROM RML_HR_EMP_LEAVE A,RML_HR_APPS_USER B
 							WHERE  A.RML_ID=B.RML_ID
-							AND A.RML_ID='$v_emp_id'
-							ORDER BY START_DATE DESC"
-                            );
+							AND A.IS_APPROVED IS NULL
+							AND B.IS_ACTIVE=1
+							AND (TRUNC(A.START_DATE) BETWEEN TO_DATE('$leave_start_date','DD/MM/YYYY') AND  TO_DATE('$leave_end_date','DD/MM/YYYY')
+							    OR 
+								TRUNC(A.END_DATE) BETWEEN TO_DATE('$leave_start_date','DD/MM/YYYY') AND  TO_DATE('$leave_end_date','DD/MM/YYYY'))
+							ORDER BY START_DATE DESC";
+							}else{
+								 $quary="SELECT B.RML_ID,
+								B.EMP_NAME,
+								B.DEPT_NAME,
+								B.BRANCH_NAME,
+								A.LEAVE_TYPE,
+								A.START_DATE,
+								A.END_DATE,
+								A.ENTRY_FROM,
+								A.IS_APPROVED,
+								B.R_CONCERN
+							FROM RML_HR_EMP_LEAVE A,RML_HR_APPS_USER B
+							WHERE  A.RML_ID=B.RML_ID
+							AND B.IS_ACTIVE=1
+							AND ('$v_emp_id' IS NULL OR A.RML_ID='$v_emp_id')
+							AND A.IS_APPROVED='$v_leave_status'
+							AND (TRUNC(A.START_DATE) BETWEEN TO_DATE('$leave_start_date','DD/MM/YYYY') AND  TO_DATE('$leave_end_date','DD/MM/YYYY')
+							    OR 
+								TRUNC(A.END_DATE) BETWEEN TO_DATE('$leave_start_date','DD/MM/YYYY') AND  TO_DATE('$leave_end_date','DD/MM/YYYY'))
+							ORDER BY START_DATE DESC";
+							}
+                           
+							//echo $quary;
+							//die();
+							$strSQL  = oci_parse($objConnect,$quary);
                             oci_execute($strSQL);
                             $number = 0;
                             while ($row = oci_fetch_assoc($strSQL)) {
@@ -111,11 +157,13 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
                                     <td><?php echo $row['RML_ID']; ?></td>
                                     <td><?php echo $row['EMP_NAME']; ?></td>
                                     <td><?php echo $row['DEPT_NAME']; ?></td>
+									<td><?php echo $row['BRANCH_NAME']; ?></td>
+									<td><?php echo $row['R_CONCERN']; ?></td>
                                     <td><?php echo $row['LEAVE_TYPE']; ?></td>
                                     <td><?php echo $row['START_DATE']; ?></td>
                                     <td><?php echo $row['END_DATE']; ?></td>
                                     <td><?php echo $row['ENTRY_FROM']; ?></td>
-                                    <td><?php echo $row['BRANCH_NAME']; ?></td>
+                                    
                                     <td><?php
                                         if ($row['IS_APPROVED'] == '1') {
                                             echo 'Approved';
@@ -146,10 +194,11 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
 								A.START_DATE,
 								A.END_DATE,
 								A.ENTRY_FROM,
-								A.IS_APPROVED
+								A.IS_APPROVED,
+								B.R_CONCERN
 							FROM RML_HR_EMP_LEAVE A,RML_HR_APPS_USER B
 							WHERE  A.RML_ID=B.RML_ID
-							AND START_DATE BETWEEN to_date((SELECT TO_CHAR(trunc(sysdate) - (to_number(to_char(sysdate,'DD')) - 1),'dd/mm/yyyy') FROM dual),'dd/mm/yyyy') AND to_date(( SELECT TO_CHAR(add_months(trunc(sysdate) - (to_number(to_char(sysdate,'DD')) - 1), 1) -1,'dd/mm/yyyy') FROM dual),'dd/mm/yyyy')
+							AND TRUNC(START_DATE) BETWEEN TO_DATE(SYSDATE,'dd/mm/RRRR')-1 AND TO_DATE(SYSDATE,'dd/mm/RRRR')
 							ORDER BY START_DATE DESC"
                             );
 
@@ -165,11 +214,13 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
                                     <td><?php echo $row['RML_ID']; ?></td>
                                     <td><?php echo $row['EMP_NAME']; ?></td>
                                     <td><?php echo $row['DEPT_NAME']; ?></td>
+									 <td><?php echo $row['BRANCH_NAME']; ?></td>
+									 <td><?php echo $row['R_CONCERN']; ?></td>
                                     <td><?php echo $row['LEAVE_TYPE']; ?></td>
                                     <td><?php echo $row['START_DATE']; ?></td>
                                     <td><?php echo $row['END_DATE']; ?></td>
                                     <td><?php echo $row['ENTRY_FROM']; ?></td>
-                                    <td><?php echo $row['BRANCH_NAME']; ?></td>
+                                   
                                     <td><?php
                                         if ($row['IS_APPROVED'] == '1') {
                                             echo 'Approved';
@@ -191,10 +242,24 @@ $emp_session_id = $_SESSION['HR']['emp_id_hr'];
                     </tbody>
                 </table>
             </div>
+			<br>
+			<div>
+			<a class="btn btn-success subbtn" id="downloadLink" onclick="exportF(this)" style="margin-left:5px;">Export to excel</a>
+			</div> 
         </div>
     </div>
     <!--/ Bordered Table -->
-
+<script>
+	function exportF(elem) {
+		  var table = document.getElementById("table");
+		  var html = table.outerHTML;
+		  var url = 'data:application/vnd.ms-excel,' + escape(html); // Set your html table into url 
+		  elem.setAttribute("href", url);
+		  elem.setAttribute("download", "Leave Information.xls"); // Choose the file name
+		  return false;
+		}
+	</script>
+	
 
 
 </div>
