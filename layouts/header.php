835ec25438a7072e86c00e6b45c8875ca516943e
joypaul1 <jyla5102555@gmail.com>
2024-01-03 18:05:22 +0600

    cache clear

--- layouts/header.php
+++ layouts/header.php
@@ -1,8 +1,8 @@
 <?php
 if (!isset($_SESSION['HR'])) {
   $currentScriptPath = __FILE__;
-  $directoryPath = dirname($currentScriptPath);
-  $includeFilePath = $directoryPath . '/../config_file_path.php';
+  $directoryPath     = dirname($currentScriptPath);
+  $includeFilePath   = $directoryPath . '/../config_file_path.php';
   //  $includeFilePath;
   // Get the real path of the included or required file
   $realIncludePath = realpath($includeFilePath);
@@ -16,45 +16,50 @@ if (!isset($_SESSION['HR'])) {
   header("Location:" . $basePath);
   exit;
 }
-$basePath =  $_SESSION['basePath'];
+$basePath = $_SESSION['basePath'];
 ?>
 <!DOCTYPE html>
-<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="assets/" data-template="vertical-menu-template-free">
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="assets/"
+  data-template="vertical-menu-template-free">
 
 <head>
-  <meta charset="utf-8" >
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" >
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
   <title>Rangs Mobile Apps Admin Panel</title>
 
-  <meta name="description" content="" >
-
-
+  <meta name="description" content="">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Pragma" content="no-cache" />
+  <meta http-equiv="Expires" content="0" />
 
   <!-- Favicon -->
   <!-- <link rel="icon" type="image/x-icon" href="<?php echo $basePath ?>/assets/img/favicon/favicon.ico" > -->
-  <link rel="icon" type="image/x-icon" href="<?php echo $basePath ?>/images/app_icon_hr.png" >
+  <link rel="icon" type="image/x-icon" href="<?php echo $basePath ?>/images/app_icon_hr.png">
 
   <!-- Fonts -->
 
-  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" >
+  <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet">
 
   <!-- Icons. Uncomment required icon fonts -->
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/fonts/boxicons.css" >
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/fonts/boxicons.css">
 
   <!-- Core CSS -->
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/core.css" class="template-customizer-core-css" >
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" >
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/css/demo.css" >
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/core.css" class="template-customizer-core-css">
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/css/theme-default.css" class="template-customizer-theme-css">
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/css/demo.css">
 
   <!-- Vendors CSS -->
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" >
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
 
-  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/apex-charts/apex-charts.css" >
+  <link rel="stylesheet" href="<?php echo $basePath ?>/assets/vendor/libs/apex-charts/apex-charts.css">
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.8/sweetalert2.min.css" integrity="sha512-y4S4cBeErz9ykN3iwUC4kmP/Ca+zd8n8FDzlVbq5Nr73gn1VBXZhpriQ7avR+8fQLpyq4izWm0b8s6q4Vedb9w==" crossorigin="anonymous" referrerpolicy="no-referrer" >
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.4.8/sweetalert2.min.css"
+    integrity="sha512-y4S4cBeErz9ykN3iwUC4kmP/Ca+zd8n8FDzlVbq5Nr73gn1VBXZhpriQ7avR+8fQLpyq4izWm0b8s6q4Vedb9w==" crossorigin="anonymous"
+    referrerpolicy="no-referrer">
   <!-- Page CSS -->
 
   <!-- Helpers -->
