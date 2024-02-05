0c31785f3fca9ff9bca1be73b3f4a0f63e3c45ae
joypaul1 <jyla5102555@gmail.com>
2024-02-05 13:05:55 +0600

    update offboarding approval

--- offboarding_module/view/lm_panel/approval_report.php
+++ offboarding_module/view/lm_panel/approval_report.php
@@ -119,39 +119,167 @@ if (!checkPermission('lm-offboarding-report')) {
 
 
                         $emp_session_id = $_SESSION['HR']['emp_id_hr'];
-                        $quary = "WITH CTE_EMP_CLEARENCE AS (
-                            SELECT 
-                                A.ID AS EMP_CLEARENCE_ID,
-                                A.RML_HR_APPS_USER_ID,
-                                A.APPROVAL_STATUS,A.CREATED_DATE
-                            FROM DEVELOPERS.EMP_CLEARENCE A
-                        )
-                        SELECT 
-                            CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID,
-                            TO_CHAR(CTE_EMP_CLEARENCE.CREATED_DATE, 'DD-MM-YYYY') as CREATED_DATE,
-                            B.EMP_NAME,
-                            B.RML_ID,
-                            B.R_CONCERN,
-                            B.DEPT_NAME,
-                            B.DESIGNATION,
-                            CTE_EMP_CLEARENCE.APPROVAL_STATUS,
-                            --LISTAGG(D.ID, ',') WITHIN GROUP (ORDER BY D.EMP_CLEARENCE_ID) AS EMP_CLEARENCE_DTLS_IDs,
-                            --LISTAGG(D.DEPARTMENT_ID, ',') WITHIN GROUP (ORDER BY D.EMP_CLEARENCE_ID) AS DEPARTMENT_IDs,
-                            LISTAGG(H.DEPT_NAME, ',') WITHIN GROUP (ORDER BY CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID) AS DEPARTMENT_NAMES,
-                            LISTAGG(COALESCE(NVL(TO_CHAR(D.APPROVE_DATE, 'DD-MM-YYYY'), '-'), ''), ',') WITHIN GROUP (ORDER BY CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID) AS APPROVE_DATES,
-                            LISTAGG(NVL(D.REMARKS,'-'), ',') WITHIN GROUP (ORDER BY D.EMP_CLEARENCE_ID) AS REMARKSS
-                        FROM CTE_EMP_CLEARENCE
-                        JOIN DEVELOPERS.RML_HR_APPS_USER B ON CTE_EMP_CLEARENCE.RML_HR_APPS_USER_ID = B.ID
-                        LEFT JOIN DEVELOPERS.EMP_CLEARENCE_DTLS D ON CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID = D.EMP_CLEARENCE_ID
-                        LEFT JOIN DEVELOPERS.RML_HR_DEPARTMENT H ON D.DEPARTMENT_ID = H.ID
-                        GROUP BY 
-                            CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID,
-                            B.EMP_NAME,
-                            B.RML_ID,
-                            B.R_CONCERN,
-                            B.DEPT_NAME,
-                            B.DESIGNATION,
-                            CTE_EMP_CLEARENCE.APPROVAL_STATUS,CTE_EMP_CLEARENCE.CREATED_DATE";
+                        $quary = "WITH
+                                    CTE_EMP_CLEARENCE AS (
+                                        SELECT
+                                            A.ID AS EMP_CLEARENCE_ID,
+                                            A.RML_HR_APPS_USER_ID,
+                                            A.APPROVAL_STATUS,
+                                            A.CREATED_DATE
+                                        FROM
+                                            DEVELOPERS.EMP_CLEARENCE A
+                                    )
+                                    SELECT
+                                        CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID,
+                                        TO_CHAR (CTE_EMP_CLEARENCE.CREATED_DATE, 'DD-MM-YYYY') AS CREATED_DATE,
+                                        B.EMP_NAME,
+                                        B.RML_ID,
+                                        B.R_CONCERN,
+                                        B.DEPT_NAME,
+                                        B.DESIGNATION,
+                                        CTE_EMP_CLEARENCE.APPROVAL_STATUS,
+                                        LISTAGG (H.DEPT_NAME, ',') WITHIN GROUP (ORDER BY CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID) AS APPROVAL_DEPARTMENT_NAMES,
+                                        --finance_accounts
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 1
+                                        ) AS FINANCE_ACCOUNTS_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 1
+                                        ) AS FINANCE_ACCOUNTS_APPROVAL_DATE,
+                                        -- Internal_Audit
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 14
+                                        ) AS INTERNAL_AUDIT_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 14
+                                        ) AS INTERNAL_AUDIT_APPROVAL_DATE,
+                                        -- Internal_Audit
+                                        -- IT_ERP_Audit
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 15
+                                        ) AS IT_ERP_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 15
+                                        ) AS INTERNAL_AUDIT_APPROVAL_DATE,
+                                        -- IT_ERP_Audit
+                                        -- Legal
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 18
+                                        ) AS LEGAL_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 18
+                                        ) AS LEGAL_APPROVAL_DATE,
+                                        -- Legal
+                                        -- Administration
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 81
+                                        ) AS ADMINISTRATION_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 81
+                                        ) AS ADMINISTRATION_APPROVAL_DATE,
+                                        -- Administration
+                                        -- Human_Resources
+                                        (
+                                            SELECT
+                                                NVL(ECD.REMARKS,'-')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 141
+                                        ) AS HUMAN_RESOURCES_REMARKS,
+                                        (
+                                            SELECT
+                                                TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                            FROM
+                                                DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                            WHERE
+                                                ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                                AND ECD.DEPARTMENT_ID = 141
+                                        ) AS HUMAN_RESOURCES_APPROVAL_DATE,
+                                        -- Human Resources
+                                        (SELECT TO_CHAR (ECD.APPROVE_DATE, 'DD-MM-YYYY')
+                                                FROM DEVELOPERS.EMP_CLEARENCE_DTLS ECD
+                                                WHERE ECD.EMP_CLEARENCE_ID = CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID
+                                            ORDER BY ECD.APPROVE_DATE DESC
+                                        FETCH FIRST 1 ROW ONLY) AS LAST_APPROVAL_DATE
+                                      
+                                    FROM
+                                        CTE_EMP_CLEARENCE
+                                        JOIN DEVELOPERS.RML_HR_APPS_USER B ON CTE_EMP_CLEARENCE.RML_HR_APPS_USER_ID = B.ID
+                                        LEFT JOIN DEVELOPERS.EMP_CLEARENCE_DTLS D ON CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID = D.EMP_CLEARENCE_ID
+                                        LEFT JOIN DEVELOPERS.RML_HR_DEPARTMENT H ON D.DEPARTMENT_ID = H.ID
+                                        --where CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID = 605
+                                    
+                                    GROUP BY
+                                        CTE_EMP_CLEARENCE.EMP_CLEARENCE_ID,
+                                        B.EMP_NAME,
+                                        B.RML_ID,
+                                        B.R_CONCERN,
+                                        B.DEPT_NAME,
+                                        B.DESIGNATION,
+                                        CTE_EMP_CLEARENCE.APPROVAL_STATUS,
+                                        CTE_EMP_CLEARENCE.CREATED_DATE";
                         $allDataSQL  = oci_parse(
                             $objConnect,
                             $quary
@@ -161,13 +289,11 @@ if (!checkPermission('lm-offboarding-report')) {
                         $number = 0;
                         while ($row = oci_fetch_assoc($allDataSQL)) {
                             $number++;
-                            $DEPARTMENT_NAME_ARRAY_DATA = (explode(",", $row['DEPARTMENT_NAMES']));
-                            $APPROVE_DATE_ARRAY_DATA    = (explode(",", $row['APPROVE_DATES']));
-                            $REMARKS_ARRAY_DATA         = (explode(",", $row['REMARKSS']));
+                            // print_r($row);
                             $startDate = date('Y-m-d', strtotime($row['CREATED_DATE']));
                             $days = 0;
                             if ($row['APPROVAL_STATUS'] == '1') {
-                                $endDate = date('Y-m-d', strtotime($APPROVE_DATE_ARRAY_DATA['5']));
+                                $endDate = date('Y-m-d', strtotime($row['LAST_APPROVAL_DATE']));
                                 $diff   = (strtotime($endDate) - strtotime($startDate));
                                 $days   = floor($diff / (60 * 60 * 24));
                             } else {
@@ -175,8 +301,6 @@ if (!checkPermission('lm-offboarding-report')) {
                                 $diff   = (strtotime($endDate) - strtotime($startDate));
                                 $days   = floor($diff / (60 * 60 * 24));
                             }
-
-
 
                         ?>
                             <tr class="text-center">
@@ -191,42 +315,46 @@ if (!checkPermission('lm-offboarding-report')) {
                                 <td> <?= date('d-m-Y', strtotime($row['CREATED_DATE'])) ?></td>
 
                                 <td>
-                                    <?php echo $APPROVE_DATE_ARRAY_DATA['0']  ?>
+                                    <?php echo isset($row['ADMINISTRATION_APPROVAL_DATE']) ? $row['ADMINISTRATION_APPROVAL_DATE'] : "-"  ?>
+
                                 </td>
                                 <td>
-                                    <?php echo $REMARKS_ARRAY_DATA['0']  ?>
+                                    <?php echo isset($row['Administration_Remarks']) ? $row['Administration_Remarks'] : "-"  ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($APPROVE_DATE_ARRAY_DATA['1']) ? $APPROVE_DATE_ARRAY_DATA['1'] : "-" ?>
+                                    <?php echo isset($row['FINANCE_ACCOUNTS_APPROVAL_DATE']) ? $row['FINANCE_ACCOUNTS_APPROVAL_DATE'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($REMARKS_ARRAY_DATA['1']) ? $REMARKS_ARRAY_DATA['1'] : "-" ?>
+                                    <?php echo isset($row['FINANCE_ACCOUNTS_REMARKS']) ? $row['FINANCE_ACCOUNTS_REMARKS'] : "-" ?>
                                 </td>
 
                                 <td>
-                                    <?php echo isset($APPROVE_DATE_ARRAY_DATA['2']) ? $APPROVE_DATE_ARRAY_DATA['2'] : "-" ?>
+                                    <?php echo isset($row['HUMAN_RESOURCES_APPROVAL_DATE']) ? $row['HUMAN_RESOURCES_APPROVAL_DATE'] : "-" ?>
+
                                 </td>
                                 <td>
-                                    <?php echo isset($REMARKS_ARRAY_DATA['2']) ? $REMARKS_ARRAY_DATA['2'] : "-" ?>
+                                    <?php echo isset($row['HUMAN_RESOURCES_REMARKS']) ? $row['HUMAN_RESOURCES_REMARKS'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($APPROVE_DATE_ARRAY_DATA['3']) ? $APPROVE_DATE_ARRAY_DATA['3'] : "-" ?>
+                                    <?php echo isset($row['INTERNAL_AUDIT_APPROVAL_DATE']) ? $row['INTERNAL_AUDIT_APPROVAL_DATE'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($REMARKS_ARRAY_DATA['3']) ? $REMARKS_ARRAY_DATA['3'] : "-" ?>
+                                    <?php echo isset($row['IT_ERP_REMARKS']) ? $row['IT_ERP_REMARKS'] : "-" ?>
+
                                 </td>
 
                                 <td>
-                                    <?php echo isset($APPROVE_DATE_ARRAY_DATA['4']) ? $APPROVE_DATE_ARRAY_DATA['4'] : "-" ?>
+                                    <?php echo isset($row['INTERNAL_AUDIT_APPROVAL_DATE']) ? $row['INTERNAL_AUDIT_APPROVAL_DATE'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($REMARKS_ARRAY_DATA['4']) ? $REMARKS_ARRAY_DATA['4'] : "-" ?>
+                                    <?php echo isset($row['INTERNAL_AUDIT_REMARKS']) ? $row['INTERNAL_AUDIT_REMARKS'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($APPROVE_DATE_ARRAY_DATA['5']) ? $APPROVE_DATE_ARRAY_DATA['5'] : "-" ?>
+                                    <?php echo isset($row['LEGAL_APPROVAL_DATE']) ? $row['LEGAL_APPROVAL_DATE'] : "-" ?>
                                 </td>
                                 <td>
-                                    <?php echo isset($REMARKS_ARRAY_DATA['5']) ? $REMARKS_ARRAY_DATA['5'] : "-" ?>
+                                    <?php echo isset($row['LEGAL_REMARKS']) ? $row['LEGAL_REMARKS'] : "-" ?>
+
                                 </td>
 
                                 <td><?= $days ?></td>
