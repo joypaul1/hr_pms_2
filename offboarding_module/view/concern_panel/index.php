f58967d044a5dbaf6e6333eea1c24af501f05850
joypaul1 <jyla5102555@gmail.com>
2024-02-05 14:46:21 +0600

    ssss

--- offboarding_module/view/concern_panel/index.php
+++ offboarding_module/view/concern_panel/index.php
@@ -3,10 +3,8 @@ require_once('../../../helper/3step_com_conn.php');
 require_once('../../../inc/connoracle.php');
 $basePath =  $_SESSION['basePath'];
 if (!checkPermission('concern-offboarding-report')) {
-
     echo "<script> window.location.href = '$basePath/index.php?logout=true'; </script>";
 }
-
 $emp_session_id = $_SESSION['HR']['emp_id_hr'];
 
 
